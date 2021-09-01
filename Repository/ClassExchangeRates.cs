89645e39b128ce0d118b84b96ba40994300bb300
discooern <72135961+discooern@users.noreply.github.com>
2021-09-01 09:02:31 +0200

    Add project files.

---
+++ Repository/ClassExchangeRates.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class ClassExchangeRates : ClassNotify
+    {
+        private string _disclaimer;
+        private string _license;
+        private long _timestamp;
+        private string _Base;
+        private Dictionary<string, double> _rates;
+
+        public ClassExchangeRates()
+        {
+            disclaimer = "";
+            license = "";
+            timestamp = 0;
+            Base = "";
+            rates = new Dictionary<string, double>();
+        }
+
+        public string disclaimer
+        {
+            get { return _disclaimer; }
+            set
+            {
+                if (_disclaimer != value)
+                {
+                    _disclaimer = value;
+                }
+                Notify("disclaimer");
+            }
+        }
+
+        public string license
+        {
+            get { return _license; }
+            set
+            {
+                if (_license != value)
+                {
+                    _license = value;
+                }
+                Notify("license");
+            }
+        }
+
+        public long timestamp
+        {
+            get { return _timestamp; }
+            set
+            {
+                if (_timestamp != value)
+                {
+                    _timestamp = value;
+                }
+                Notify("timestamp");
+            }
+        }
+
+        public string Base
+        {
+            get { return _Base; }
+            set
+            {
+                if (_Base != value)
+                {
+                    _Base = value;
+                }
+                Notify("Base");
+            }
+        }
+
+        public Dictionary<string, double> rates
+        {
+            get { return _rates; }
+            set
+            {
+                if (_rates != value)
+                {
+                    _rates = value;
+                }
+                Notify("rates");
+            }
+        }
+
+    }
+}
---
+++ Repository/ClassRates.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class ClassRates : ClassNotify
+    {
+        private string _key;
+        private double _value;
+
+        public ClassRates()
+        {
+            key = "";
+            value = 0D;
+        }
+
+        public double value
+        {
+            get { return _value; }
+            set
+            {
+                if (_value != value)
+                {
+                    _value = value;
+                }
+                Notify("value");
+            }
+        }
+
+        public string key
+        {
+            get { return _key; }
+            set
+            {
+                if (_key != value)
+                {
+                    _key = value;
+                }
+                Notify("key");
+            }
+        }
+
+    }
+}
